3993abb98981b00945990edbeb962e5fe556af6b
khusanov-95 <fa.khusano@gmail.com>
2024-03-09 14:05:14 +0500

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+console.log(123);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(4000);
