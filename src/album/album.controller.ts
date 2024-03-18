a2ff19678763c61d331f0cde65432e4834bcdde9
khusanov-95 <fa.khusano@gmail.com>
2024-03-18 09:27:19 +0500

    add album

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Put,
+  Delete,
+  Param,
+} from '@nestjs/common';
+import { AlbumService } from './album.service';
+import { CreateAlbumDto } from './dtos/create-album.dto';
+import { UpdateAlbumDto } from './dtos/update-album.dto';
 
 @Controller('album')
-export class AlbumController {}
+export class AlbumController {
+  constructor(private albumService: AlbumService) {}
+
+  @Get('/:id')
+  findAlbum(@Param('id') id: string) {
+    return this.albumService.findOne(id);
+  }
+
+  @Get()
+  findAllAlbums() {
+    return this.albumService.find();
+  }
+
+  @Post()
+  createAlbum(@Body() body: CreateAlbumDto) {
+    const { name, year, artistId } = body;
+    this.albumService.create(name, year, artistId);
+  }
+
+  @Put('/:id')
+  updateAlbum(@Param('id') id: string, @Body() body: UpdateAlbumDto) {
+    this.albumService.update(id, body);
+  }
+
+  @Delete('/:id')
+  removeAlbum(@Param('id') id: string) {
+    return this.albumService.remove(id);
+  }
+}
--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { albumDB } from 'src/db/fakeDB';
+import { v4 as uuidv4 } from 'uuid';
+import { UpdateAlbumDto } from './dtos/update-album.dto';
+import { Album } from './album.interface';
 
 @Injectable()
-export class AlbumService {}
+export class AlbumService {
+  create(name, year, artistId) {
+    const id = uuidv4();
+
+    const newAlbum = {
+      id,
+      name,
+      year,
+      artistId,
+    };
+
+    albumDB.push(newAlbum);
+    return newAlbum;
+  }
+
+  update(id: string, body: UpdateAlbumDto) {
+    const album = albumDB.find((album) => album.id === id);
+
+    if (!album) {
+      throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+    }
+
+    Object.assign(album, body);
+
+    return album;
+  }
+
+  find(): Album[] {
+    return albumDB;
+  }
+
+  findOne(id: string): Album {
+    return albumDB.find((user) => user.id === id);
+  }
+
+  remove(id: string) {
+    const albumIndex = albumDB.findIndex((album) => album.id === id);
+
+    if (albumIndex) {
+      albumDB.splice(albumIndex, 1);
+      return true;
+    }
+
+    throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+  }
+}
